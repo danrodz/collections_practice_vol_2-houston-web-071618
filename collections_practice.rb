16323b324ddcc609addc4215233bd3344fe0e729
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-25 03:44:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -65,6 +65,50 @@ def merge_data(collection1, collection2)
   new_collection.flatten
 end
 
+let(:keys) {
+    [
+           {
+            :first_name => "blake"
+        },
+           {
+            :first_name => "ashley"
+        }
+    ]
+  }
+
+  let(:data) {
+    [
+           {
+             "blake" => {
+                :awesomeness => 10,
+                     :height => "74",
+                  :last_name => "johnson"
+            },
+            "ashley" => {
+                :awesomeness => 9,
+                     :height => 60,
+                  :last_name => "dubs"
+            }
+        }
+    ]
+  }
+
+  let(:merged_data) {
+    [
+           {
+             :first_name => "blake",
+            :awesomeness => 10,
+                 :height => "74",
+              :last_name => "johnson"
+        },
+           {
+             :first_name => "ashley",
+            :awesomeness => 9,
+                 :height => 60,
+              :last_name => "dubs"
+        }
+    ]
+  }
 
 
 
